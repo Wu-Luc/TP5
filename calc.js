f78df681f1e2f9182839c187a465e69b2a248d95
Wu-Luc <106806572+Wu-Luc@users.noreply.github.com>
2024-11-07 10:33:26 +0100

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,28 @@
+function addition(js){
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number (a)+Number(b);
+
+    t3.value =c;
+}
+
+function permute(){
+    var v3 ="";
+    a= document.getElementById("t1").value;
+    b= document.getElementById("t2").value;
+    v3 =a;
+    a= b;
+    b= v3;
+
+    t1.value = a;
+    t2.value = b;
+}
+
+function parite(){
+    var v;
+    v = Number(document.getElementById("t3"));
+    if(v%2==0)
+    t4.value = ("Pair")
+    else
+    t4.value= ("Impair")
+}
